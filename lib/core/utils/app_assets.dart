e44912f30f197693f78385355a74eb7586e1e5be
AyaElShafey <aya.elshafey110@gmail.com>
2024-10-23 22:23:59 +0300

    App_Colors

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,32 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+
+  /// Assets for imagesForget
+  /// assets/images/forget.png
+  static const String imagesForget = "assets/images/forget.png";
+
+  /// Assets for imagesMasged
+  /// assets/images/masged.svg
+  static const String imagesMasged = "assets/images/masged.svg";
+
+  /// Assets for imagesOnboarding1
+  /// assets/images/onboarding1.png
+  static const String imagesOnboarding1 = "assets/images/onboarding1.png";
+
+  /// Assets for imagesOnboarding2
+  /// assets/images/onboarding2.png
+  static const String imagesOnboarding2 = "assets/images/onboarding2.png";
+
+  /// Assets for imagesOnboarding3
+  /// assets/images/onboarding3.png
+  static const String imagesOnboarding3 = "assets/images/onboarding3.png";
+
+  /// Assets for imagesPyramids
+  /// assets/images/pyramids.svg
+  static const String imagesPyramids = "assets/images/pyramids.svg";
+
+  /// Assets for imagesSuccces
+  /// assets/images/succces.png
+  static const String imagesSuccces = "assets/images/succces.png";
+}
